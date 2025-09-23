9695152a2c563a629593b93f332f5ae2a94e0b26
Rachel Mareraki <rjm.mareraki@gmail.com>
2025-09-24 08:43:59 +1200

    bug: routing loop

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,11 @@ function App() {
   return (
     <>
       <div>
+        {/* After doing some research, This code is trying to route through itself. In the main.jsx file 
+        there is another route configuration linked to our pages causing an error. An easy fix is import 
+        router provider from react-router-dom and the router from the router.jsx file then in the return 
+        remove the current code and replace it with the router provider component with the router config passed as props
+        */}
         <BrowserRouter>
           <App />
         </BrowserRouter>
