4115b593416da9627f6f2711c83576a88e4b1da5
Dhanisha Harshad <dhanisha.harshad@gmail.com>
2025-09-24 09:07:06 +1200

    feat: refactor and organize controller and service files; remove unused imports

---
+++ backend/src/controllers/stationRoutes.js
@@ -0,0 +1,8 @@
+// import React from "react";
+// import express from "express";
+// import router from "express.Router";
+// import ZEnergyStation from "../models/ZEnergySchema.js";
+
+// export default function stationRoutes() {
+//   return <div></div>;
+// }
---
+++ backend/src/services/stationFilter.js
@@ -0,0 +1,6 @@
+import ZEnergyStation from "../models/ZEnergySchema";
+import mongoose from "mongoose";
+
+export async function filterStations(filters) {
+  const query = {};
+}
