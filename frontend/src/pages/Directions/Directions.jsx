f36bcccd6906516cae09ef1d2b56da4a568dc0cf
christine <christinec@missionreadyhq.com>
2025-09-24 13:18:38 +1200

    feat: set up skeleton structure of directions.jsx

--- frontend/src/pages/Directions/Directions.jsx
+++ frontend/src/pages/Directions/Directions.jsx
@@ -1,5 +1,44 @@
-// import React from "react";
+import React from "react";
 
 export default function Directions() {
-  return <div></div>;
+  return (
+    <div className="directionsWrapper">
+      {/* nav bar */}
+      {/* TODO: this should conditional render station name */}
+      {/* Main Content */}
+      <main className="directionsStationCard">
+        station name here
+        {/* -------- LEFT SECTION -------- */}
+        <section className="directionsLeftSection">
+          left section
+          <div className="directionsSearchWrapper">
+            <input
+              type="text"
+              placeholder="Search"
+              className="directionsSearchInput"
+              required
+            />
+            <input
+              type="text"
+              placeholder="Station Address"
+              className="directionsSearchInput"
+              required
+            />
+          </div>
+          <div>fuel</div>
+          <div>hours</div>
+          <div>service</div>
+        </section>
+        {/* -------- RIGHT SECTION -------- */}
+        <section className="directionsRightSection">
+          right section - map
+        </section>
+      </main>
+
+      {/* -------- CTA -------- */}
+      <aside> CTA here </aside>
+
+      {/* -------- FOOTER -------- */}
+    </div>
+  );
 }
