7c3bfe2f9131a15e76c20597b8100f591542922d
Rachel Mareraki <rjm.mareraki@gmail.com>
2025-09-26 03:10:15 +1200

    feat: added stationCard to find station page

--- frontend/src/pages/FindStation/FindStation.jsx
+++ frontend/src/pages/FindStation/FindStation.jsx
@@ -1,28 +1,32 @@
-import styles from "./FindStation.module.css"
-
+import styles from "./FindStation.module.css";
+import StationCard from "../../shared/stationCard/StationCard";
 export default function FindStation() {
-  
   return (
-    <div className={styles.findStationPage}>
-      <header><h1>Header</h1></header>
+    <main className={styles.findStationPage}>
+      <header>
+        <h1>Header</h1>
+      </header>
       <div className={styles.pageName}>
         <h3>Find Station</h3>
       </div>
-      <main className={styles.content}>
+      <section className={styles.content}>
         <section className={styles.stationContainer}>
-        <form className={styles.searchBar}>
-          <label>Search Placeholder</label>
-          <input type="text" placeholder="search location"/>
-          <img src="/assets/filters/D-FilterDefault.png" alt="filter button" />
-        </form>
-        <section className={styles.stationCards}>
-          <h1>Station Card Container</h1>
-        </section>
+          <form className={styles.searchBar}>
+            <label>Search Placeholder</label>
+            <input type="text" placeholder="search location" />
+            <img
+              src="/assets/filters/D-FilterDefault.png"
+              alt="filter button"
+            />
+          </form>
+          <section className={styles.stationCards}>
+            <StationCard />
+          </section>
         </section>
         <section className={styles.mapContainer}>
           <h3>MAP</h3>
         </section>
-      </main>
-    </div>
-  )
+      </section>
+    </main>
+  );
 }
