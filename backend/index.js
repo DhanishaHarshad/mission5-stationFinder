692c65d138a512874083327d847043f285dad651
Rachel Mareraki <rjm.mareraki@gmail.com>
2025-09-26 12:01:17 +1200

    feat: set up server connection, and morgan dependency

--- backend/index.js
+++ backend/index.js
@@ -2,3 +2,19 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import morgan from "morgan";
+import { connectDB } from "./src/config/connectDb.js";
+
+dotenv.config()
+const app = express();
+const PORT = process.env.PORT
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+connectDB()
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`)
+})
