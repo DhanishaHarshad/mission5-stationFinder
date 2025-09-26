6ec0789e000f461fa76f9d98a02aadc780e22616
Dhanisha Harshad <dhanisha.harshad@gmail.com>
2025-09-26 17:59:29 +1200

    refactor: clean up Header component by removing commented-out code and adjusting navigation layout

--- frontend/src/shared/header/Header.jsx
+++ frontend/src/shared/header/Header.jsx
@@ -8,33 +8,14 @@ import CroppedHeaderImage from "../../../public/assets/images/CroppedHeader.png"
 function Header() {
   return (
     <div className={Styles.header}>
-      {/* logo */}
-      {/* <img src={ZEnergyLogo} alt="Z Energy Logo" className="zEnergyLogo" /> */}
-
-      {/* top right */}
-      {/* <div className={Styles.innerHeader}>
-        <div className={Styles.topRight}>
-          <ul>
-            <li>
-              <p>About Z</p>
-            </li>
-            <li>
-              <p>Z App</p>
-            </li>
-          </ul>
-        </div>
-      </div> */}
-
-      {/* <line className={Styles.line} /> */}
       <img src={CroppedHeaderImage} alt="Header Image" className="zEnergyNav" />
       {/* bottom left navigation */}
       <div className={Styles.bottomLeft}>
         <a></a>
         <a>At The Station</a>
-
         <a>Rewards & Promotions</a>
-
         <a href="/find-station">Find Station</a>
+        <a></a>
         <a></a>
       </div>
     </div>
