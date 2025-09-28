0d8c500a8c4dc542781242ed219f81087122a6eb
christine <christinec@missionreadyhq.com>
2025-09-29 10:16:23 +1300

    chore: add comment to section out the page and wrap backIcon with input row for better alignment on css

--- frontend/src/pages/Directions/Directions.jsx
+++ frontend/src/pages/Directions/Directions.jsx
@@ -7,48 +7,61 @@ import plusIconButton from "/assets/icons/misc/AddDefault.png";
 export default function Directions({ selectedStation }) {
   return (
     <div className={styles.directionsWrapper}>
-      {/* nav bar */}
+      {/* ---------------------------------------------- */}
+      {/*                    NAV BAR                     */}
+      {/* ---------------------------------------------- */}
       <nav> {/* TODO: add navbar link here*/} </nav>
 
-      {/* Main Content */}
+      {/* ---------------------------------------------- */}
+      {/*                      BODY                      */}
+      {/* ---------------------------------------------- */}
       <main className={styles.directionsBody}>
-        {/* conditional render / onclick - not hardcoded */}
         <div className={styles.directionsStationNameWrapper}>
           <h1 className={styles.directionsStationName}>
             {selectedStation?.name || "Selected Station"}
           </h1>
         </div>
-        {/* -------- LEFT SECTION -------- */}
+        {/* ---------------------------------------------- */}
+        {/*                 LEFT SECTION                   */}
+        {/* ---------------------------------------------- */}
         <div className={styles.directionsSectionWrapper}>
           <section className={styles.directionsLeftSection}>
             <div className={styles.directionsSearchWrapper}>
-              {/* TODO: on click it will bring back to find station */}
-              <button className={styles.diectionsBackIconButton}>
-                <img
-                  src={backIconButton}
-                  alt="back-icon"
-                  className={styles.directionsIconImg}
+              {/* "back" icon and search bar */}
+              <div className={styles.directionsInputRow}>
+                <div className={styles.directionsBackIconWrapper}>
+                  <button className={styles.directionsBackIconButton}>
+                    <img
+                      src={backIconButton}
+                      alt="back-icon"
+                      className={styles.directionsIconImg}
+                    />
+                  </button>
+                </div>
+                <input
+                  type="text"
+                  placeholder="Search"
+                  className={styles.directionsSearchInput}
+                  required
                 />
-              </button>
-              <input
-                type="text"
-                placeholder="Search"
-                className={styles.directionsSearchInput}
-                required
-              />
-              <input
-                type="text"
-                placeholder="Station Address"
-                className={styles.directionsSearchInput}
-                required
-              />
-              <button className={styles.directionsPlusIconButton}>
-                <img
-                  src={plusIconButton}
-                  alt="plus-icon"
-                  className={styles.directionsIconImg}
+              </div>
+
+              {/* "plus" icon + station address */}
+              <div className={styles.directionsInputRow}>
+                <input
+                  type="text"
+                  placeholder="Station Address"
+                  className={styles.directionsSearchInput}
+                  required
                 />
-              </button>
+                <button className={styles.directionsPlusIconButton}>
+                  <img
+                    src={plusIconButton}
+                    alt="plus-icon"
+                    className={styles.directionsIconImg}
+                  />
+                </button>
+              </div>
             </div>
             {/* TODO: import station cards here */}
             {/* station card */}
@@ -56,19 +69,25 @@ export default function Directions({ selectedStation }) {
               station card here
             </div>
           </section>
-          {/* -------- RIGHT SECTION -------- */}
+          {/* ---------------------------------------------- */}
+          {/*                RIGHT SECTION                   */}
+          {/* ---------------------------------------------- */}
           <section className={styles.directionsRightSection}>
             <Map />
           </section>
         </div>
       </main>
 
-      {/* -------- CTA -------- */}
+      {/* ---------------------------------------------- */}
+      {/*                      CTA                       */}
+      {/* ---------------------------------------------- */}
       <aside className={styles.directionsCTAWrapper}>
         {/* TODO: Add CTA links*/}
       </aside>
 
-      {/* -------- FOOTER -------- */}
+      {/* ---------------------------------------------- */}
+      {/*                     FOOTER                     */}
+      {/* ---------------------------------------------- */}
       <footer className={styles.directionsFooter}>
         {/* TODO: Add footer links */}
       </footer>
