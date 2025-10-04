f8878471c4f8b0222a0523165164950726ca4f19
Rachel Mareraki <rjm.mareraki@gmail.com>
2025-10-05 01:17:13 +1300

    note: steps to implement search in useStationResults.js hook in the frontend

--- frontend/src/hooks/UseStationResults.js
+++ frontend/src/hooks/UseStationResults.js
@@ -1,12 +1,16 @@
 import { useEffect, useState } from "react";
 const apiUrl = import.meta.env.VITE_STATION_API
 
+//_______ FOR SEARCH: UPDATE THE HOOK TO ACCEPT searchQuery & selectedFilters AS ARGUMENTS _____
+
 export const useStationResults = () => {
 
   const [stations, setStations] = useState([])
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
 
+
+  //___ FOR SEARCH: BUILD THE QUERY STRING FROM searchQuery & selectedFilters INSIDE USEEFFECT
   useEffect(() => {
     const getStations = async () => {
       try {
@@ -23,6 +27,8 @@ export const useStationResults = () => {
     }
     getStations()
   }, [apiUrl])
+
+  //___ FOR SEARCH: RETURN THE HOOK DATA ____
 
   return { stations, loading, error }
 }
