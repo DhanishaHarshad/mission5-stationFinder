b401e9191f4fe8d7c35aaa375750a90c8c805cbb
Rachel Mareraki <rjm.mareraki@gmail.com>
2025-10-05 20:59:22 +1300

    chore: added comments to explain certain parts of my code.

--- frontend/src/shared/filter/FilterDropdown.jsx
+++ frontend/src/shared/filter/FilterDropdown.jsx
@@ -7,6 +7,7 @@ export default function FilterDropdown(props) {
   const [selectedCheckbox, setSelectedCheckbox] = useState([]);
   const isExpanded = Object.values(openCategories).some(Boolean);
 
+  //____ TOGGLE THE CATEGORY DROPDOWN ____
   const toggleCategory = (category) => {
     setOpenCategories((prev) => ({
       ...prev,
@@ -14,6 +15,7 @@ export default function FilterDropdown(props) {
     }));
   };
 
+  //___ TOGGLE THE CHECKBOX ___
   const toggleCheckbox = (item) => {
     setSelectedCheckbox((prev) =>
       prev.includes(item)
@@ -22,16 +24,28 @@ export default function FilterDropdown(props) {
     );
   };
 
+  //____ APPLY BUTTON HANDLER ____
   const onApplyFilter = () => {
     props.onApplyFilter(selectedCheckbox);
   };
 
+
+  //____ CLEAR FILTERS BUTTON HANDLER _____
+    const onClearFilter = () => {
+    setSelectedCheckbox([]);
+    props.onApplyFilter([]); 
+  };
+
   return (
+
+    //___ WHEHN THE FILTER CONTAINER IS OPEN A LIST OF CATEGORIES WITH DROPDOWNS ARE RENDERED.
+    //...  WHEN A CATEGORY DROPDOWN IS CLICKED, EACH CATEGORY RETURNS A CHECKBOX ICON, SERVICE NAME, LIST OF GROUPED SERVICES AND A STYLE CLASSNAME.
+    //... THE CHECKBOX IMAGES ARE CONDITIONALLY RENDERED BETWEEN ACTIVE AND INACTIVE, ON CLICK.
     <div
       className={`${styles.filterContainer} ${
         isExpanded ? styles.expanded : ""
       }`}
-    >
+    > 
       {filterData.map(({ category, icon, items, style }) => {
         const isOpen = openCategories[category];
 
@@ -67,8 +81,9 @@ export default function FilterDropdown(props) {
           </ul>
         );
       })}
+      {/* ____ THESE ARE THE BUTTONS THAT WILL CLEAR FILTERS OR APPLY FILTERS _____ */}
       <div className={styles.filterButtons}>
-        <button className={styles.clearBtn}>
+        <button className={styles.clearBtn} onClick={onClearFilter}>
           Clear Filter ( {selectedCheckbox.length} )
         </button>
         <button className={styles.applyBtn} onClick={onApplyFilter}>
